a4e016e2ba6c9fa83b2f53b27961c7639e97a1c8
Frederik Agger <frederikagger@hotmail.com>
2019-11-28 09:19:48 +0100

    skabelon til projekt

---
+++ src/main/java/ejerforening/firstyearprojektkea/FirstyearprojektkeaApplication.java
@@ -0,0 +1,13 @@
+package ejerforening.firstyearprojektkea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstyearprojektkeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstyearprojektkeaApplication.class, args);
+    }
+
+}
