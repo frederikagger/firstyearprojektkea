686f5d85abac16f2ad2a67d1888be9980c86a178
Frederik Agger <frederikagger@hotmail.com>
2019-11-28 10:57:12 +0100

    lejlighedsController klassen er oprettet

--- src/main/java/ejerforening/firstyearprojektkea/Controller/LejlighedsController.java
+++ src/main/java/ejerforening/firstyearprojektkea/Controller/LejlighedsController.java
@@ -1,4 +1,16 @@
 package ejerforening.firstyearprojektkea.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LejlighedsController {
+
+    @GetMapping("/lejligheder")
+    public String Lejligheder(Model model){
+        model.addAttribute("lejligheder", lejlighedService.hentAlle);
+        return "lejligheder";
+
+    }
 }
