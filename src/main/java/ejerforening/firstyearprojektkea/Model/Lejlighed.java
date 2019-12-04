c2522e3a28367914f33b74e1b48e96c691de25ed
Frederik Agger <frederikagger@hotmail.com>
2019-12-04 08:41:16 +0100

    en import fjernet

--- src/main/java/ejerforening/firstyearprojektkea/Model/Lejlighed.java
+++ src/main/java/ejerforening/firstyearprojektkea/Model/Lejlighed.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  * Oprettet af Frederik Agger
