1634978a9627aa9c63c435a2709f8fdb790f7194
Nobralyra <signees96@gmail.com>
2019-12-04 22:28:04 +0100

    Udfyldt dele af Service

--- src/main/java/ejerforening/firstyearprojektkea/Service/SlutbrugerService.java
+++ src/main/java/ejerforening/firstyearprojektkea/Service/SlutbrugerService.java
@@ -1,4 +1,31 @@
 package ejerforening.firstyearprojektkea.Service;
 
-public class SlutbrugerService {
+import ejerforening.firstyearprojektkea.Model.AdministreSlutbruger.SlutbrugerOversigt;
+import ejerforening.firstyearprojektkea.Repository.ISlutbrugerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SlutbrugerService implements ISlutbrugerService {
+
+    @Autowired
+    private ISlutbrugerRepository slutbrugerRepository;
+
+    @Override
+    public List<SlutbrugerOversigt> hentAlle()
+    {
+        List<SlutbrugerOversigt> result = slutbrugerRepository.hentAlle();
+        return result;
+    }
+
+    @Override
+    public void slet(int slutbrugerId)
+    {
+        slutbrugerRepository.slet(slutbrugerId);
+    }
+
 }
