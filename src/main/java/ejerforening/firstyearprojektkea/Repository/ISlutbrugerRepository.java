d5ed89ec648e0d8dd157c5c95db3769a96d79a0a
Nobralyra <signees96@gmail.com>
2019-12-04 22:26:35 +0100

    Interface for slutbruger repository

--- src/main/java/ejerforening/firstyearprojektkea/Repository/ISlutbrugerRepository.java
+++ src/main/java/ejerforening/firstyearprojektkea/Repository/ISlutbrugerRepository.java
@@ -1,4 +1,11 @@
 package ejerforening.firstyearprojektkea.Repository;
 
+import ejerforening.firstyearprojektkea.Model.AdministreSlutbruger.SlutbrugerOversigt;
+
+import java.util.List;
+
 public interface ISlutbrugerRepository {
+    List<SlutbrugerOversigt> hentAlle();
+
+    void slet(int slutbrugerId);
 }
