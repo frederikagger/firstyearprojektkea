e73c568ba2615651ec51256da9f71e8dc53180ba
Nobralyra <signees96@gmail.com>
2019-12-04 17:30:20 +0100

    Clean up

---
+++ src/main/java/ejerforening/firstyearprojektkea/Controller/SlutbrugerController.java
@@ -0,0 +1,18 @@
+package ejerforening.firstyearprojektkea.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SlutbrugerController
+{
+    /**
+     *Getmetode, som er det foerste man rammer naar man tilgaar hjemmesiden
+     * @return htmlsiden forside
+     */
+    @GetMapping("/template")
+    public String visSlutbrugerForside()
+    {
+        return "/slutbruger/slutbrugerforside";
+    }
+}
