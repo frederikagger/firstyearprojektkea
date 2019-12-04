d2dc02466048ed5dedcf627f080841e588f726b9
Nobralyra <signees96@gmail.com>
2019-12-04 22:23:55 +0100

    Navnet er ændret fra SlutbrugerController til AdministrereSlutbrugereController Getmappings over de undersider der hører til bestyrelsesmedlem rettigheder

--- src/main/java/ejerforening/firstyearprojektkea/Controller/AdministrereSlutbrugereController.java
+++ src/main/java/ejerforening/firstyearprojektkea/Controller/AdministrereSlutbrugereController.java
@@ -1,18 +1,53 @@
 package ejerforening.firstyearprojektkea.Controller;
 
+import ejerforening.firstyearprojektkea.Model.AdministreSlutbruger.SlutbrugerOversigt;
+import ejerforening.firstyearprojektkea.Service.ISlutbrugerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class SlutbrugerController
+public class AdministrereSlutbrugereController
 {
+    @Autowired
+    private ISlutbrugerService slutbrugerService;
+
     /**
      *Getmetode, som er det foerste man rammer naar man tilgaar hjemmesiden
      * @return htmlsiden forside
      */
-    @GetMapping("/slutbrugerforside")
-    public String visSlutbrugerForside()
+    @GetMapping("/slutbruger/oversigt")
+    public String oversigt(Model model)
     {
-        return "/slutbruger/slutbrugerforside";
+        List<SlutbrugerOversigt> result = slutbrugerService.hentAlle();
+        model.addAttribute("oversigt", result);
+        return "/slutbruger/oversigt";
     }
+
+    @GetMapping("/slutbruger/slet")
+    public String slet(Model model, int slutbrugerId)
+    {
+        slutbrugerService.slet(slutbrugerId);
+        List<SlutbrugerOversigt> result = slutbrugerService.hentAlle();
+        model.addAttribute("oversigt", result);
+        return "/slutbruger/oversigt";
+    }
+
+    /*
+    @GetMapping("/slutbruger/opdater")
+    public String opdater()
+    {
+        return "/slutbruger/opdater";
+    }
+
+    @GetMapping("/slutbruger/detaljer")
+    public String detaljer()
+    {
+        return "/slutbruger/detaljer";
+    }
+
+     */
 }
