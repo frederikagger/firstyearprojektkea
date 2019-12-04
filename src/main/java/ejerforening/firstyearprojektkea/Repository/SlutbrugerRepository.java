1003057d9d0db7531c37105dcbb44d1cd9bee454
Nobralyra <signees96@gmail.com>
2019-12-04 22:27:55 +0100

    Udfyldt dele af repository

--- src/main/java/ejerforening/firstyearprojektkea/Repository/SlutbrugerRepository.java
+++ src/main/java/ejerforening/firstyearprojektkea/Repository/SlutbrugerRepository.java
@@ -1,4 +1,34 @@
 package ejerforening.firstyearprojektkea.Repository;
 
-public class SlutbrugerRepository {
+import ejerforening.firstyearprojektkea.Model.AdministreSlutbruger.SlutbrugerOversigt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SlutbrugerRepository implements ISlutbrugerRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<SlutbrugerOversigt> hentAlle()
+    {
+        String sql = "SELECT CONCAT(slutbruger.fornavn, ' ', slutbruger.mellemnavn, ' ', slutbruger.efternavn) AS navn, slutbruger.slutbrugerid, slutbrugeroplysninger.lejlighedsid " +
+                "FROM slutbruger INNER JOIN slutbrugeroplysninger ON slutbruger.slutbrugerid = slutbrugeroplysninger.slutbrugeroplysid;";
+        RowMapper<SlutbrugerOversigt> rowmapper = new BeanPropertyRowMapper<>(SlutbrugerOversigt.class);
+        List<SlutbrugerOversigt> result  = jdbcTemplate.query(sql, rowmapper);
+        return result;
+    }
+
+    @Override
+    public void slet(int slutbrugerId)
+    {
+        String sql = "DELETE FROM slutbruger WHERE slutbruger.slutbrugerid = ?;";
+        jdbcTemplate.update(sql, slutbrugerId);
+    }
 }
