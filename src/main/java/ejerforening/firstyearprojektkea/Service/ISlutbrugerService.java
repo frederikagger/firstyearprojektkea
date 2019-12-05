0c8baf399b9e7d94bca7be75d1273de79ecd3c73
Nobralyra <signees96@gmail.com>
2019-12-05 20:22:17 +0100

    Ny linjer før {

--- src/main/java/ejerforening/firstyearprojektkea/Service/ISlutbrugerService.java
+++ src/main/java/ejerforening/firstyearprojektkea/Service/ISlutbrugerService.java
@@ -1,10 +1,11 @@
 package ejerforening.firstyearprojektkea.Service;
 
-import ejerforening.firstyearprojektkea.Model.AdministreSlutbruger.SlutbrugerOversigt;
+import ejerforening.firstyearprojektkea.Model.AdministrereSlutbruger.SlutbrugerOversigt;
 
 import java.util.List;
 
-public interface ISlutbrugerService {
+public interface ISlutbrugerService
+{
     List<SlutbrugerOversigt> hentAlle();
 
     void slet(int slutbrugerId);
