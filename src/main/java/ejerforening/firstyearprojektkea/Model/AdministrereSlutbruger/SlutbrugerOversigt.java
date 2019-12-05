7c6e786c751412480aa64adf65dd37c50c065f16
Nobralyra <signees96@gmail.com>
2019-12-05 20:26:28 +0100

    Tilføjet kommentar og validerings annotationer

--- src/main/java/ejerforening/firstyearprojektkea/Model/AdministrereSlutbruger/SlutbrugerOversigt.java
+++ src/main/java/ejerforening/firstyearprojektkea/Model/AdministrereSlutbruger/SlutbrugerOversigt.java
@@ -1,8 +1,22 @@
 package ejerforening.firstyearprojektkea.Model.AdministrereSlutbruger;
 
+import javax.persistence.Id;
 
+/**
+ * Klassen er den model der vises ud til htmlsiden, saa man kan se det som en model view klasse.
+ * Det er de felter bestyrelsen har brug for at se, for at kunne vaelge hvem de vil rette, se detaljer eller slette.
+ * Tabellerne fra databasen der vises er opgave og lejlighed.
+ * @Author Signe
+ */
 public class SlutbrugerOversigt
 {
+
+    /**
+     * Attributerne har validerings annotationerm som definerer:
+     *
+     * @Id = primary key i tabellen
+    */
+    @Id
     private int slutbrugerId;
     private String navn;
     private int lejlighedsId;
