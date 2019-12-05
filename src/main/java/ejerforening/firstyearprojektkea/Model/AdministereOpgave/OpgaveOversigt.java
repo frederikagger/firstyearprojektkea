2d47abdebb40a9fbfafd02750022f7214a071c10
Nobralyra <signees96@gmail.com>
2019-12-05 20:23:36 +0100

    Tilføjet kommentar

--- src/main/java/ejerforening/firstyearprojektkea/Model/AdministereOpgave/OpgaveOversigt.java
+++ src/main/java/ejerforening/firstyearprojektkea/Model/AdministereOpgave/OpgaveOversigt.java
@@ -1,14 +1,22 @@
 package ejerforening.firstyearprojektkea.Model.AdministereOpgave;
 
+import javax.persistence.Id;
+
 /**
  * Klassen er den model der vises ud til htmlsiden, saa man kan se det som en model view klasse.
  * Det er de felter bestyrelsen har brug for at se, for at kunne vaelge hvem de vil rette, se detaljer eller slette.
- * Tabellerne fra databasen den skal vise er opgave og lejlighed.
+ * Tabellerne fra databasen der vises er opgave og lejlighed.
  * @Author Signe
  */
 
 public class OpgaveOversigt
 {
+    /**
+     * Attributerne har validerings annotationerm som definerer:
+     *
+     * @Id = primary key i tabellen
+     */
+    @Id
     private int opgaveId;
     private String navn;
     private int lejlighedsId;
