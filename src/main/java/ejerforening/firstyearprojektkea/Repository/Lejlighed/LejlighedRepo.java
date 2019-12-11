96ec887527ea7f50ead773772ad6d64109ef5770
Frederik Agger <frederikagger@hotmail.com>
2019-12-11 08:20:33 +0100

    lejlighedRepo ændret

--- src/main/java/ejerforening/firstyearprojektkea/Repository/Lejlighed/LejlighedRepo.java
+++ src/main/java/ejerforening/firstyearprojektkea/Repository/Lejlighed/LejlighedRepo.java
@@ -21,27 +21,33 @@ public class LejlighedRepo implements IlejlighedRepo {
 
     @Override
     public List<Lejlighed> hentAlle() {
-        String sql = "SELECT * FROM lejlighed";
+        String sql = "SELECT * FROM database_first_year_projekt.lejlighed";
         RowMapper<Lejlighed> rowmapper = new BeanPropertyRowMapper<>(Lejlighed.class);
         return jdbcTemplate.query(sql, rowmapper);
     }
 
     @Override
-    public Lejlighed findMedId(int id){
-        String sql = "SELECT * FROM lejlighed WHERE id = ?";
+    public Lejlighed findMedId(int lejlighedsId){
+        String sql = "SELECT * FROM database_first_year_projekt.lejlighed WHERE lejlighedsId = ? LIMIT 1";
         RowMapper<Lejlighed> rowMapper = new BeanPropertyRowMapper<>(Lejlighed.class);
-        return jdbcTemplate.queryForObject(sql,rowMapper,id);
+        return jdbcTemplate.queryForObject(sql,rowMapper, lejlighedsId);
     }
 
     @Override
     public void opret(Lejlighed lejlighed) {
-        String sql = "INSERT INTO lejlighed (etage,lejlighedsside) VALUES(?,?)";
+        String sql = "INSERT INTO database_first_year_projekt.lejlighed (etage,lejlighedsside) VALUES(?,?)";
         jdbcTemplate.update(sql,lejlighed.getEtage(),lejlighed.isLejlighedsside());
     }
 
     @Override
     public void sletLejlighed(int id){
-        String sql = "DELETE FROM lejlighed WHERE lejlighedsid = ?";
+        String sql = "DELETE FROM database_first_year_projekt.lejlighed WHERE lejlighedsid = ?";
         jdbcTemplate.update(sql,id);
+    }
+
+    @Override
+    public void opdaterLejlighed(Lejlighed lejlighed) {
+        String sql = "UPDATE database_first_year_projekt.lejlighed SET ETAGE = ? , LEJLIGHEDSSIDE=? WHERE lejlighedsId=?";
+        jdbcTemplate.update(sql,lejlighed.getEtage(), lejlighed.isLejlighedsside(),lejlighed.getLejlighedsid());
     }
 }
