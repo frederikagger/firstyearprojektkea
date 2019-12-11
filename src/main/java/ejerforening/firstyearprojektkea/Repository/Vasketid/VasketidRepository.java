c2d35c88fdcfba0846f9e3468da243182d593761
Frederik Agger <frederikagger@hotmail.com>
2019-12-11 08:20:02 +0100

    vasketidsrepo ændret

--- src/main/java/ejerforening/firstyearprojektkea/Repository/Vasketid/VasketidRepository.java
+++ src/main/java/ejerforening/firstyearprojektkea/Repository/Vasketid/VasketidRepository.java
@@ -1,6 +1,10 @@
 package ejerforening.firstyearprojektkea.Repository.Vasketid;
 
+import ejerforening.firstyearprojektkea.Model.AdministrereSlutbruger.Slutbruger;
+import ejerforening.firstyearprojektkea.Model.Lejlighed.Lejlighed;
 import ejerforening.firstyearprojektkea.Model.Vasketider.Vasketid;
+import org.apache.catalina.User;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -23,8 +27,19 @@ public class VasketidRepository implements IVasketidRepository {
     }
 
     @Override
-    public boolean book() {
-        return false;
+    public Vasketid findbyVaskeId(int vaskeid) {
+        String sql = "SELECT * FROM database_first_year_projekt.vasketid WHERE vaskeId = ? LIMIT 1";
+        RowMapper<Vasketid> rowMapper = new BeanPropertyRowMapper<>(Vasketid.class);
+        return jdbcTemplate.queryForObject(sql,rowMapper,vaskeid);
+    }
+
+    @Override
+    public void book(Vasketid vasketid){
+        //String sql = "UPDATE vasketid SET erBooketEllerEj=TRUE, lejlighedsId=? WHERE vaskeId=?";
+       // jdbcTemplate.update(sql, vasketid.getLejlighedsId(), vasketid.getVaskeId());
+        String sql = "update vasketid set erBooketEllerEj=true, lejlighedsid=? where vaskeId=?";
+        System.out.println(vasketid.getLejlighedsId()+" "+ vasketid.getVaskeId());
+        jdbcTemplate.update(sql,vasketid.getLejlighedsId(),vasketid.getVaskeId());
     }
 
     @Override
