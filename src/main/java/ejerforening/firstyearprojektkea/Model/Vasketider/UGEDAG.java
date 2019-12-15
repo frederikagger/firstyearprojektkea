e345688200d61529d3168e7213cdd907a5521abe
Frederik Agger <frederikagger@hotmail.com>
2019-12-15 17:47:46 +0100

    javadocs tilføjet

--- src/main/java/ejerforening/firstyearprojektkea/Model/Vasketider/UGEDAG.java
+++ src/main/java/ejerforening/firstyearprojektkea/Model/Vasketider/UGEDAG.java
@@ -1,5 +1,11 @@
 package ejerforening.firstyearprojektkea.Model.Vasketider;
 
+/**
+ * @author frede
+ * Denne enum står med småt fordi vi viser dem direkte i et view
+ * og det er flottere end med caps lock.
+ */
+
 public enum UGEDAG {
     mandag,
     tirsdag,
